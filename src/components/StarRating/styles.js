ccbc66ee9ffd9feb63c927f3fc41c0a4e57f540f
Ahavaz <lucas.cesar.8@hotmail.com>
2019-03-02 05:51:50 -0300

    Adds star rating component styles

---
+++ src/components/StarRating/styles.js
@@ -0,0 +1,16 @@
+import { StyleSheet } from 'react-native'
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    flexDirection: 'row',
+    marginBottom: 30
+  },
+
+  starButton: {
+    paddingLeft: 5,
+    paddingRight: 5
+  }
+})
+
+export default styles
