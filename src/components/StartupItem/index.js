01bb0dc1cdb118011a8452911f432f535c94faf9
Ahavaz <lucas.cesar.8@hotmail.com>
2019-03-02 05:49:46 -0300

    Adds startup icon component

---
+++ src/components/StartupItem/index.js
@@ -0,0 +1,42 @@
+import React from 'react'
+import {
+  Alert,
+  Text,
+  View,
+  Image,
+  TouchableNativeFeedback,
+  TouchableOpacity,
+  Platform
+} from 'react-native'
+
+import styles from './styles'
+
+const StartupItem = ({ data, navigation }) => {
+  const onPressItem = data => {
+    navigation.navigate('Details', { data })
+  }
+
+  return Platform.OS === 'android' ? (
+    <TouchableNativeFeedback
+      onPress={() => onPressItem(data)}
+      underlayColor="transparent"
+      background={TouchableNativeFeedback.SelectableBackground()}
+    >
+      <View style={styles.container}>
+        <Image style={styles.image} source={{ uri: data.imageUrl }} />
+        <Text style={styles.startup}>{data.name}</Text>
+        <Text style={styles.segment}>{data.Segment.name}</Text>
+      </View>
+    </TouchableNativeFeedback>
+  ) : (
+    <TouchableOpacity onPress={() => onPressItem(data)} underlayColor="transparent">
+      <View style={styles.container}>
+        <Image style={styles.image} source={{ uri: data.imageUrl }} />
+        <Text style={styles.startup}>{data.name}</Text>
+        <Text style={styles.segment}>{data.Segment.name}</Text>
+      </View>
+    </TouchableOpacity>
+  )
+}
+
+export default StartupItem
