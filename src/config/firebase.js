27180c768dbf403c9c57ee2b35abe41b42843060
Ahavaz <lucas.cesar.8@hotmail.com>
2019-03-02 05:47:24 -0300

    Adds firebase config

---
+++ src/config/firebase.js
@@ -0,0 +1,10 @@
+const config = {
+  apiKey: 'AIzaSyDF_X-VJPE8rwDY2ynCEIR9OlJO70HRkpo',
+  authDomain: 'startup-fest.firebaseapp.com',
+  databaseURL: 'https://startup-fest.firebaseio.com',
+  projectId: 'startup-fest',
+  storageBucket: 'startup-fest.appspot.com',
+  messagingSenderId: '1056820651413'
+}
+
+export default config
