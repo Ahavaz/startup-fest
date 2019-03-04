ce415f5ca40f23b089f30aafe0720a7a3fb4c21b
Ahavaz <lucas.cesar.8@hotmail.com>
2019-03-04 04:00:58 -0300

    Adjusts padding to better fit smaller screens

--- src/pages/Details/styles.js
+++ src/pages/Details/styles.js
@@ -13,8 +13,8 @@ const styles = StyleSheet.create({
 
   container: {
     flex: 1,
-    paddingLeft: '15%',
-    paddingRight: '15%'
+    paddingLeft: '10%',
+    paddingRight: '10%'
   },
 
   item: {
